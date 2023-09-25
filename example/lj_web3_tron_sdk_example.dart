52a1e9f00bcd995fefb6cdfff75fd34dadc7c249
JamesBondMine <lujia58@outlook.com>
2023-09-25 13:48:39 +0800

    项目创建
    项目创建

---
+++ example/lj_web3_tron_sdk_example.dart
@@ -0,0 +1,6 @@
+import 'package:lj_web3_tron_sdk/lj_web3_tron_sdk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/lj_web3_tron_sdk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library lj_web3_tron_sdk;
+
+export 'src/lj_web3_tron_sdk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
